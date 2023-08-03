f9a8fee19987cca749e46feb5b661a545162a98f
lakshmanan2004 <124753145+lakshmanan2004@users.noreply.github.com>
2023-08-03 13:49:19 +0530

    Update exp_program-8.py

--- exp_program-8.py
+++ exp_program-8.py
@@ -1,33 +1,24 @@
 import pandas as pd
 
-# Sample data for demonstration
-data = {
-    'location': ['Location_A', 'Location_B', 'Location_A', 'Location_C', 'Location_B', 'Location_C', 'Location_A'],
-    'listing_price': [500000, 700000, 600000, 800000, 750000, 900000, 550000],
-    'bedrooms': [3, 4, 3, 5, 6, 4, 5],
-    'area': [1200, 1500, 1300, 1800, 2000, 1600, 1400]
+# Sample sales data in the format of a dictionary with product names as keys and quantities sold as values
+sales_data = {
+    "Product A": 100,
+    "Product B": 80,
+    "Product C": 120,
+    "Product D": 150,
+    "Product E": 90,
+    "Product F": 60,
+    # Add more products and their sales data here
 }
 
-# Creating the DataFrame from the sample data
-property_data = pd.DataFrame(data)
+# Create a DataFrame from the sales_data dictionary
+df = pd.DataFrame(list(sales_data.items()), columns=["Product", "Quantity Sold"])
 
-def main():
-    # 1. Average listing price of properties in each location
-    average_listing_price = property_data.groupby('location')['listing_price'].mean()
-    print("Average Listing Price of Properties in Each Location:")
-    print(average_listing_price)
-    print()
+# Sort the DataFrame by Quantity Sold in descending order
+df_sorted = df.sort_values(by="Quantity Sold", ascending=False)
 
-    # 2. Number of properties with more than four bedrooms
-    properties_with_more_than_four_bedrooms = property_data[property_data['bedrooms'] > 4]
-    number_of_properties_with_more_than_four_bedrooms = properties_with_more_than_four_bedrooms.shape[0]
-    print("Number of Properties with More Than Four Bedrooms:", number_of_properties_with_more_than_four_bedrooms)
-    print()
+# Retrieve the top 5 products
+top_5_products = df_sorted.head(5)
 
-    # 3. Property with the largest area
-    property_with_largest_area = property_data.loc[property_data['area'].idxmax()]
-    print("Property with the Largest Area:")
-    print(property_with_largest_area)
-
-if __name__ == "__main__":
-    main()
+print("Top 5 products sold the most in the past month:")
+print(top_5_products)
